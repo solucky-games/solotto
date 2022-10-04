0af8d2c65b31ce327144bdf5e47d6a8395505f85
quantium <alexcollsoutumuro@gmail.com>
2022-10-04 16:10:56 +0200

    v0.5

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script>
+
 import { ref, computed, watchEffect } from 'vue'
 import { useLocalStorage } from '@vueuse/core'
 import { Connection, PublicKey, Keypair, clusterApiUrl, SystemProgram, Transaction } from '@solana/web3.js'
 import { Program, AnchorProvider } from '@project-serum/anchor'
 import { WalletMultiButton, useAnchorWallet, useWallet } from 'solana-wallets-vue'
 import idl from './idl.json'
-import { Buffer } from 'buffer';
+import { Buffer } from 'buffer'
 
 // @ts-ignore
 window.Buffer = Buffer;
@@ -13,6 +14,9 @@ window.Buffer = Buffer;
 
 const programID = new PublicKey(idl.metadata.address)
 const preflightCommitment = 'processed'
+
+const masterWallet = '6i1zfRMWVEErVPkH4JUtEBj5PFk2VZgshAENhZi1Dj1k'
+const network = 'devnet'
 
 
 export default {
@@ -23,20 +27,24 @@ export default {
     const dark = ref(false)
     const wallet = useAnchorWallet()
     
-    const connection = new Connection(clusterApiUrl('devnet'), preflightCommitment)
+    const connection = new Connection(clusterApiUrl(network), preflightCommitment)
     const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
 
     const counterPublicKey = useLocalStorage('counterPublicKey', null);
     const counter = ref(0);
+    const prize = ref(0);
     const balance = ref();
 
     watchEffect(async () => {
       if (!counterPublicKey.value) return;
       const account = await program.value.account.baseAccount.fetch(counterPublicKey.value)
       counter.value = account.count.toNumber()
+      const pri = await connection.getBalance(new PublicKey(masterWallet))/1000000000;
+      prize.value = Math.floor(pri*100)/100;
       const bal = await connection.getBalance(wallet.value.publicKey)/1000000000;
       balance.value = Math.floor(bal*100)/100;
+      
     })
 
 
@@ -80,12 +88,12 @@ export default {
         return alert('Connect your wallet first.')
       } 
 
-      const connection = new Connection(clusterApiUrl('devnet'), preflightCommitment)
+      const connection = new Connection(clusterApiUrl(network), preflightCommitment)
 
       const transaction = new Transaction().add(
           SystemProgram.transfer({
               fromPubkey: wallet.value.publicKey,
-              toPubkey: new PublicKey('6i1zfRMWVEErVPkH4JUtEBj5PFk2VZgshAENhZi1Dj1k'),
+              toPubkey: new PublicKey(masterWallet),
               lamports: 1000000000 })
       )
 
@@ -96,8 +104,8 @@ export default {
       const bal = await connection.getBalance(wallet.value.publicKey)/1000000000;
       balance.value = Math.floor(bal*100)/100;
 
-      this.$refs.myId.innerText = this.balance;
-      console.log(this.$refs.myId.innerText);
+      const pri = await connection.getBalance(new PublicKey(masterWallet))/1000000000;
+      prize.value = Math.floor(pri*100)/100;
 
     };
 
@@ -108,7 +116,9 @@ export default {
       createCounter,
       incrementCounter,
       balance,
-      sendSOL
+      prize,
+      sendSOL,
+      masterWallet
     }
   },
 }
@@ -122,7 +132,7 @@ export default {
 
       <!-- Solana Wallets Vue. -->
       <wallet-multi-button :dark="dark"></wallet-multi-button>
-      <p id="myId" ref="myId" :class="dark ? 'text-white' : 'text-gray-600'" class="text-s font-semibold mt-3">{{`${balance} SOL`}}</p>
+      <p :class="dark ? 'text-white' : 'text-gray-600'" class="text-sm font-semibold mt-3" v-if="balance">{{`${balance} SOL`}}</p>
 
       <!-- Dark Button. -->
       <button @click="dark = !dark" class="rounded-full p-3" :class="dark ? 'bg-white/10 hover:bg-white/20 text-gray-200' : 'bg-black/10 hover:bg-black/20 text-gray-600'">
@@ -140,28 +150,20 @@ export default {
     <div class="m-auto w-full max-w-md p-8">
       <div class="shadow-xl rounded-xl" :class="dark ? 'bg-gray-700' : 'bg-white'">
         
-        
         <div class="p-8 text-center">
-          <p class="uppercase text-xs tracking-widest text-gray-400 font-semibold">Counter</p>
-          <p
-            class="font-bold text-5xl mt-2"
-            :class="dark ? 'text-white' : 'text-gray-900'"
-            v-text="counterPublicKey ? counter : 'Not Set'"
-          ></p>
+          <p class="uppercase text-xs tracking-widest text-gray-400 font-semibold">Total prize</p>
+          <div class="flex justify-center mr-3" >
+            <p class="font-bold text-2xl mt-5 mr-1"
+              :class="dark ? 'text-white' : 'text-gray-900'"
+            >◎ </p>
+            <p class="font-bold text-5xl mt-2"
+              :class="dark ? 'text-white' : 'text-gray-900'"
+            > {{prize}}</p>
+          </div>
         </div>
 
 
         <div class="flex">
-          <button
-            class="flex-1 py-4 px-2 rounded-bl-xl"
-            :class="dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'"
-            @click="createCounter"
-          >New Counter</button>
-          <button
-            class="flex-1 py-4 px-2 rounded-br-xl"
-            :class="dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'"
-            @click="incrementCounter"
-          >Increment Counter</button>
           <button
             class="flex-1 py-4 px-2 rounded-br-xl"
             :class="dark ? 'hover:bg-gray-800' : 'hover:bg-gray-100'"
@@ -171,10 +173,10 @@ export default {
       </div>
 
       <div class="text-sm mt-8">
-        <p class="text-xs font-semibold text-gray-400">Wallet address:</p>
+        <p class="text-xs font-semibold text-gray-400">Your wallet:</p>
         <p>{{ $wallet.publicKey.value?.toBase58() ?? 'Not connected' }}</p>
-        <p class="text-xs font-semibold text-gray-400 mt-4">Counter address:</p>
-        <p>{{ counterPublicKey ?? 'Not created' }}</p>
+        <p class="text-xs font-semibold text-gray-400 mt-4">Master wallet:</p>
+        <p>{{ masterWallet ?? 'Not created' }}</p>
       </div>
     </div>
   </div>
