e71815610d24cfcb4f08610e7669d83685b7da2f
quantium <alexcollsoutumuro@gmail.com>
2022-11-12 15:41:04 +0100

    finished PotPanel & PlayPanel

--- app/server/sql/CREATE TABLE "_$luckies$_" .sql
+++ app/server/sql/CREATE TABLE "_$luckies$_" .sql
@@ -1,11 +1,17 @@
-CREATE TABLE "_$luckies$_" ( 
-  __date__ varchar(21) PRIMARY KEY, 
-  _num int,
-  _verified boolean,
-  _owner varchar(255),
-  _flag varchar(11), 
-  _pot int,
-  _timestamp timestamp
-);
+-- CREATE TABLE "_$luckies$_" ( 
+--   __date__ varchar(25) PRIMARY KEY,
+--   _time varchar(25),
+--   _num int,
+--   _verified boolean,
+--   _account varchar(255),
+--   _owner varchar(255),
+--   _flag varchar(11), 
+--   _pot int,
+--   _timestamp bigint
+-- );
 
 -- DROP TABLE "_$luckies$_";
+
+INSERT INTO _$luckies$_  ( __date__, _time, _num, _verified, _account, _owner, _flag, _pot, _timestamp ) 
+VALUES ( '2022-11-06', '14:03:00', 783434, true, 'rjeiurfrefhuhrfejreh439034njkn', '2TyAp92s7TEksnycmYY2Fk5i1j5anwFTqECyuFMVhomP', '🇪🇸', 120, 7984798437 )
+
--- app/server/sql/CREATE TABLE "_ticketSchema_".sql
+++ app/server/sql/CREATE TABLE "_ticketSchema_".sql
@@ -2,6 +2,7 @@ CREATE TABLE "_ticketSchema_" (
   _hour varchar(21),
   __num__ int PRIMARY KEY,
   _verified boolean,
+  _account varchar(255),
   _owner varchar(255),
   _flag varchar(11), 
   _pot int,
